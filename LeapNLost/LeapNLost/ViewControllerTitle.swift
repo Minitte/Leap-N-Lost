6f04c503349c5e086c31b9f2536ee28e6335f5b7
Ricky Mok <rickymok@rickys-mac.local>
2019-01-31 20:08:59 -0800

    Added simple title screen and main menu

---
+++ LeapNLost/LeapNLost/ViewControllerTitle.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LeapNLost
+//
+//  Created by Ricky Mok on 2019-01-31.
+//  Copyright © 2019 bcit. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTitle: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Animates Fade In and Fade Out on button
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseIn, .autoreverse, .repeat, .allowUserInteraction
+            ], animations: {
+            self.startButton.alpha = 1.0
+        }, completion: nil)    }
+
+
+}
+
