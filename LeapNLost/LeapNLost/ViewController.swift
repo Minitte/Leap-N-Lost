cfe2005f4a3a60f2c821d5029ceba2578e07e5d4
Ricky Mok <rickymok@rickys-mac.local>
2019-01-31 16:28:28 -0800

    basic app start

---
+++ LeapNLost/LeapNLost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeapNLost
+//
+//  Created by Ricky Mok on 2019-01-31.
+//  Copyright © 2019 bcit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
