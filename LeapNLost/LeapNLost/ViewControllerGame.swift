2d54c51ae0044364768e64605143bdc9b2cf2276
Davis Pham <a00962164@imac-10.ad.bcit.ca>
2019-02-04 17:21:08 -0800

    basic view for game

---
+++ LeapNLost/LeapNLost/ViewControllerGame.swift
@@ -0,0 +1,19 @@
+//
+//  ViewControllerGame.swift
+//  LeapNLost
+//
+//  Created by Davis Pham on 2019-02-04.
+//  Copyright © 2019 bcit. All rights reserved.
+//
+
+import Foundation
+import GLKit
+
+class ViewControllerGame : GLKViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSLog("Hello World")
+    }
+}
