611223c1688d6746cb4847ad307308ddbc416cc7
Davis Pham <a00962164@imac-10.ad.bcit.ca>
2019-02-04 18:47:43 -0800

    Added red cube base code.

---
+++ LeapNLost/LeapNLost/Engine/Renderer.mm
@@ -0,0 +1,119 @@
+//
+//  Copyright © Borna Noureddin. All rights reserved.
+//
+
+#import "Renderer.h"
+#import <Foundation/Foundation.h>
+#import <GLKit/GLKit.h>
+#include <chrono>
+#include "GLESRenderer.hpp"
+
+// Uniform index.
+enum
+{
+    UNIFORM_MODELVIEWPROJECTION_MATRIX,
+    NUM_UNIFORMS
+};
+GLint uniforms[NUM_UNIFORMS];
+
+// Attribute index.
+enum
+{
+    ATTRIB_VERTEX,
+    NUM_ATTRIBUTES
+};
+
+@interface Renderer () {
+    GLKView *theView;
+    GLESRenderer glesRenderer;
+    GLuint programObject;
+    std::chrono::time_point<std::chrono::steady_clock> lastTime;
+
+    GLKMatrix4 mvp;
+
+    float *vertices;
+    int *indices, numIndices;
+}
+
+@end
+
+@implementation Renderer
+
+- (void)dealloc
+{
+    glDeleteProgram(programObject);
+}
+
+- (void)loadModels
+{
+    numIndices = glesRenderer.GenSquare(1.0f, &vertices, &indices);
+}
+
+- (void)setup:(GLKView *)view
+{
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+    
+    if (!view.context) {
+        NSLog(@"Failed to create ES context");
+    }
+    
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    theView = view;
+    [EAGLContext setCurrentContext:view.context];
+    if (![self setupShaders])
+        return;
+
+    glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+    glEnable(GL_DEPTH_TEST);
+    lastTime = std::chrono::steady_clock::now();
+}
+
+- (void)update
+{
+    auto currentTime = std::chrono::steady_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastTime).count();
+    lastTime = currentTime;
+    
+    // Perspective
+    mvp = GLKMatrix4Translate(GLKMatrix4Identity, 0.0, 0.0, -5.0);
+
+    float aspect = (float)theView.drawableWidth / (float)theView.drawableHeight;
+    GLKMatrix4 perspective = GLKMatrix4MakePerspective(60.0f * M_PI / 180.0f, aspect, 1.0f, 20.0f);
+
+    mvp = GLKMatrix4Multiply(perspective, mvp);
+}
+
+- (void)draw:(CGRect)drawRect;
+{
+    glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, FALSE, (const float *)mvp.m);
+
+    glViewport(0, 0, (int)theView.drawableWidth, (int)theView.drawableHeight);
+    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glUseProgram ( programObject );
+
+    glVertexAttribPointer ( 0, 3, GL_FLOAT,
+                           GL_FALSE, 3 * sizeof ( GLfloat ), vertices );
+    glEnableVertexAttribArray ( 0 );
+    glVertexAttrib4f ( 1, 1.0f, 0.0f, 0.0f, 1.0f );
+    glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, FALSE, (const float *)mvp.m);
+    glDrawElements ( GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, indices );
+}
+
+
+- (bool)setupShaders
+{
+    // Load shaders
+    char *vShaderStr = glesRenderer.LoadShaderFile([[[NSBundle mainBundle] pathForResource:[[NSString stringWithUTF8String:"Shader.vsh"] stringByDeletingPathExtension] ofType:[[NSString stringWithUTF8String:"Shader.vsh"] pathExtension]] cStringUsingEncoding:1]);
+    char *fShaderStr = glesRenderer.LoadShaderFile([[[NSBundle mainBundle] pathForResource:[[NSString stringWithUTF8String:"Shader.fsh"] stringByDeletingPathExtension] ofType:[[NSString stringWithUTF8String:"Shader.fsh"] pathExtension]] cStringUsingEncoding:1]);
+    programObject = glesRenderer.LoadProgram(vShaderStr, fShaderStr);
+    if (programObject == 0)
+        return false;
+    
+    // Set up uniform variables
+    uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX] = glGetUniformLocation(programObject, "modelViewProjectionMatrix");
+
+    return true;
+}
+
+@end
+
--- LeapNLost/LeapNLost/ViewControllerGame.swift
+++ LeapNLost/LeapNLost/ViewControllerGame.swift
@@ -8,12 +8,32 @@
 
 import Foundation
 import GLKit
+import Swift
 
-class ViewControllerGame : GLKViewController {
+class ViewControllerGame : GLKViewController, GLKViewControllerDelegate {
+    
+    var renderer: Renderer = Renderer()
+    var ozma: GLKView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.delegate = self
         
-        NSLog("Hello World")
+        renderer = Renderer()
+        var viewz = self.view as? GLKView
+        
+        renderer.setup(self.view as? GLKView)
+        
+        renderer.loadModels()
+        
+    }
+    
+    func glkViewControllerUpdate(_ controller: GLKViewController) {
+        renderer.update()
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        renderer.draw(rect)
     }
 }
