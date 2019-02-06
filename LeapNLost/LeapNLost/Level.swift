4ae61bae5e9fdd7ccfd859c9519d10334dd4182e
Ricky Mok <rickymok@rickys-mac.local>
2019-02-06 01:37:20 -0800

    Made title page tap anywhere to start and got JSON file reading to work

--- LeapNLost/LeapNLost/Level.swift
+++ LeapNLost/LeapNLost/Level.swift
@@ -9,73 +9,77 @@
 import Foundation;
 
 struct Level : Decodable {
-    struct Info : Decodable {
-        let area: Int;
-        let level: Int;
-        let desc: String;
-        
-        // If you want the fields to be a specific name
-        enum CodingKeys : String, CodingKey {
-            case area;
-            case level
-            case desc
-        }
-        
-        // Info Constructor
-        init(){
-            area = -1;
-            level = -1;
-            desc = "";
-        }
-    }
-    
-    struct Row : Decodable {
-        let id: Int;
-        let type: String;
-        let speed: Float;
-        
-        // Row constructor
-        init(){
-            id = -1;
-            type = "";
-            speed = 0.0;
-        }
-    }
-    
-    let info: Info;
-    let rows: [Row];
-    
+    var info: Info;
+    var rows: [Row];
     // Level Constructor
     init(){
         info = Info();
         rows = [Row]();
     }
-}
-
-//Reads the level file for Area area and Level level i.e. Level 1-1 readLevel(1,1)
-func readLevel(withArea area: Int, withLevel level: Int) -> Data{
-    let path = "Levels\\Level_" + String(area) + "-" + String(level) + ".json";
-    var data = Data();
-    do {
-        data =  try JSONSerialization.data(withJSONObject: path);
-    }
-    catch {
-        print("Error reading file.")
-    }
-    
-    return data;
-}
-
-// Accepts a JSON string as argument, returns a Level object
-func parseJSON(data: Data) -> Level{
-    var level = Level();
-    do {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        level = try decoder.decode(Level.self, from: data)
-    } catch{
-        print("Error parsing JSON.")
+    //Reads the level file for Area area and Level level i.e. Level 1-1 readLevel(1,1)
+    func readLevel(withArea area: Int, withLevel level: Int) -> Data{
+        //let pathS = "Levels/Level_" + String(area) + "-" + String(level);
+        let path = Bundle.main.path(forResource: "Level_" + String(area) + "-" + String(level), ofType: "json", inDirectory: "Levels");
+        
+        var result = Data();
+        do {
+            result = try Data(contentsOf: URL(fileURLWithPath: path!));
+        }
+        catch {
+            print("Error reading file. \(error)");
+        }
+        
+        return result;
     }
     
-    return level;
+    // Accepts a JSON string as argument, returns a Level object
+    func parseJSON(data: Data) -> Level{
+        var level = Level();
+        do {
+            let decoder = JSONDecoder();
+            decoder.keyDecodingStrategy = .convertFromSnakeCase;
+            level = try decoder.decode(Level.self, from: data);
+        } catch{
+            print("Error parsing JSON. \(error)");
+        }
+        
+        return level;
+    }
 }
+
+// Info struct
+struct Info : Decodable {
+    let area: Int;
+    let level: Int;
+    let desc: String;
+    
+    // If you want the fields to be a specific name
+    /*enum CodingKeys : String, CodingKey {
+        case area;
+        case level
+        case desc
+    }*/
+    
+    // Info Constructor
+    init(){
+        area = -1;
+        level = -1;
+        desc = "";
+    }
+}
+
+// Row struct
+struct Row : Decodable {
+    let id: Int;
+    let type: String;
+    let speed: Double;
+    
+    // Row constructor
+    init(){
+        id = -1;
+        type = "";
+        speed = 0.0;
+    }
+}
+
+
