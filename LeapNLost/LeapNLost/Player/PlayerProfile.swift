86a71ef2b1efdc24ddef423563a766dcdc90b2fd
Davis Pham <a00962164@imac-10.ad.bcit.ca>
2019-02-06 14:18:10 -0800

    Load and saving json

--- LeapNLost/LeapNLost/Player/PlayerProfile.swift
+++ LeapNLost/LeapNLost/Player/PlayerProfile.swift
@@ -50,4 +50,71 @@ class PlayerProfile : Codable {
         lastLevel = ll;
         scoreboard = sb;
     }
+    
+    /**
+     * Writes the save to file
+     */
+    public func saveToFile() {
+        
+        var payload : Data?;
+        
+        do {
+            payload = try JSONEncoder().encode(self);
+        } catch {
+            NSLog("Failed to encode player profile");
+        }
+        
+        let payloadStr : String! = String(data: payload!, encoding: .utf8)!;
+        
+        //this is the file. we will write to and read from it
+        let file = "playerprofile0.sav";
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            
+            let fileURL = dir.appendingPathComponent(file);
+            
+            //writing
+            do {
+                try payloadStr.write(to: fileURL, atomically: false, encoding: .utf8);
+            } catch {
+                /* error handling here */
+                
+            }
+        }
+    }
+    
+    /**
+     * Tries to load a Player Profile from local storage
+     * If loading a profile is successful, it is returned
+     */
+    public static func loadFromFile() -> PlayerProfile? {
+        
+        //this is the file. we will write to and read from it
+        let file = "playerprofile0.sav";
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            
+            let fileURL = dir.appendingPathComponent(file);
+            
+            //reading
+            do {
+                let text = try String(contentsOf: fileURL, encoding: .utf8);
+                
+                let profileData : Data? = text.data(using: .utf8);
+                
+                if (profileData == nil) {
+                    NSLog("Error reading a player profile!");
+                }
+                
+                let loadedProfile : PlayerProfile = try JSONDecoder().decode(PlayerProfile.self, from: profileData!);
+                
+                return loadedProfile;
+                
+            } catch {
+                NSLog("Error reading a player profile!");
+            }
+        }
+        
+        return nil;
+    }
 }
