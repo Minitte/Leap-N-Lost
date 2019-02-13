0c7d74ba8997092f6268db304410e8a9b7cda3a7
Davis Pham <a00962164@dhcp-142-232-233-203.ad.bcit.ca>
2019-02-13 13:56:20 -0800

    cach manager

---
+++ LeapNLost/LeapNLost/Util/ModelCachManager.swift
@@ -0,0 +1,56 @@
+//
+//  ModelCachManager.swift
+//  LeapNLost
+//
+//  Created by Davis Pham on 2019-02-13.
+//  Copyright © 2019 bcit. All rights reserved.
+//
+
+import Foundation
+import GLKit;
+
+class ModelCachManager {
+    
+    public static var instance : ModelCachManager = ModelCachManager.init();
+    
+    private var meshDictionary : [String : MeshSet];
+    
+    init() {
+        meshDictionary = [:];
+    }
+    
+    /**
+     * returns a model with the given texture and mesh.
+     * If the mesh has not been loaded yet, this will try to load it from file.
+     */
+    public func loadModel(withMeshName meshName:String, withTextureName texName:String) -> Model? {
+        
+        var mesh : MeshSet? = meshDictionary[meshName];
+        
+        // mesh not found in cach, try to load a new one
+        if (mesh == nil) {
+            NSLog("Mesh " + meshName + " not founnd in cach, loading a new one");
+            
+            mesh = OBJLoader.loadMeshSet(nameOfMesh: meshName);
+            
+            // check if still null
+            if (mesh == nil) {
+                return nil;
+            }
+        }
+        
+        let model : Model = Model.init(vertices: (mesh?.vertices)!, indices: (mesh?.indices)!);
+        
+//        model.loadTexture(filename: texName);
+        
+        return model;
+    }
+}
+
+struct MeshSet {
+    
+    var vertices : [Vertex];
+    
+    var indices : [GLuint];
+    
+}
