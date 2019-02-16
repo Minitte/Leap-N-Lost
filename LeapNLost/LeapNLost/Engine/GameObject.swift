44b38316d4637a9abb455cf63eaa7bda1c38944b
Ricky Mok <20971047+MunifiSense@users.noreply.github.com>
2019-02-16 03:14:15 -0800

    Fixed origin on car and made them move across the screen

--- LeapNLost/LeapNLost/Engine/GameObject.swift
+++ LeapNLost/LeapNLost/Engine/GameObject.swift
@@ -27,6 +27,12 @@ class GameObject {
     // Scale of this game object.
     var scale : Vector3;
     
+    // Game object type
+    var type : String;
+    
+    // Game object speed
+    var speed : Float;
+    
     /**
      * Construtor for the game object.
      * model - the model of the game object.
@@ -38,13 +44,41 @@ class GameObject {
         position = Vector3(Float.random(in: -2...2), Float.random(in: -2...2), Float.random(in: -2...2));
         rotation = Vector3(0, 0, 0);
         scale = Vector3(0.5, 0.5, 0.5);
+        type = "";
+        speed = 1.0;
     }
     
     /**
      * Update loop.
      */
     func update(delta: Float) {
-        // Continuously rotate around y axis for testing purposes
-        rotation.y += 1 * delta;
+        
+        // What type of game object is this?
+        switch(type){
+        // Game object is a car
+        case "car":
+            // If speed is positive, move right
+            if(speed > 0.0){
+                rotation.y = -1.5708;
+                if(position.x < Float(Level.tilesPerRow)/2){
+                    position.x += 1 * delta * speed;
+                } else {
+                    position.x = Float(-Level.tilesPerRow);
+                }
+            }
+            // If speed is negative, move left
+            else{
+                rotation.y = 1.5708;
+                if(position.x > Float(-Level.tilesPerRow)/2){
+                    position.x += 1 * delta * speed;
+                } else {
+                    position.x = Float(Level.tilesPerRow);
+                }
+            }
+        default:
+            // Continuously rotate around y axis for testing purposes
+            //rotation.y += 1 * delta;
+            break;
+        }
     }
 }
