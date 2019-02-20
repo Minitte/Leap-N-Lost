7792b7adbaf8d2e2ecff1c6f5942d979a1c5861c
Davis Pham <a00962164@imac-10.ad.bcit.ca>
2019-02-19 17:36:17 -0800

    movement with a tap

--- LeapNLost/LeapNLost/Engine/PlayerGameObject.swift
+++ LeapNLost/LeapNLost/Engine/PlayerGameObject.swift
@@ -10,35 +10,25 @@ import Foundation
 
 class PlayerGameObject : GameObject {
  
-    private var gravity : Vector3 = Vector3.init(0, -9.81, 0);
+    private var gravity : Vector3 = Vector3.init(0, -9.81 * 3, 0);
     
-    private var leap : Vector3 = Vector3.init(1, 3, 0);
+    private var leap : Vector3 = Vector3.init(0, 5, 1);
     
     private var velocity : Vector3 = Vector3.init(0, 0, 0);
     
-    private var time : Float = 0;
-    
-    private var hopTime : Float = 0;
-    
     private var hopping : Bool = false;
     
-    private var frameTime : Float = 1/60;
+    private var frameTime : Float = 1/30;
     
     /**
      * Overrided base update
      */
     override func update(delta: Float) {
-        time += frameTime;
-        
-        if (!hopping && time > 2) {
-            time = 0;
-            
+        if (InputManager.singleTap && !hopping) {
             hopForward();
         }
         
         if (hopping) {
-            hopTime += frameTime;
-            
             velocity = velocity + (gravity * frameTime);
             
             let scaledVelocity : Vector3 = velocity * frameTime;
@@ -46,15 +36,13 @@ class PlayerGameObject : GameObject {
             position = position + scaledVelocity;
             
             if (position.y < 0) {
+                position.y = 0;
                 hopping = false;
-                time = 0;
             }
         }
     }
     
     public func hopForward() {
-        hopTime = 0;
-        
         velocity = leap * 1;
         
         hopping = true;
