96c25b68e242fe2ca1cabb3b3dd128c0fc2307a9
Anthony Wong <awong6201@gmail.com>
2019-03-26 17:40:23 -0700

    Offset each car's position

--- LeapNLost/LeapNLost/Engine/Scene/Theme/City.swift
+++ LeapNLost/LeapNLost/Engine/Scene/Theme/City.swift
@@ -27,7 +27,7 @@ class City : Theme {
             let sectionWidth : Float = Float(Level.tilesPerRow) / 3.0 * 2.0;
             
             for i in 1...3 {
-                gameObjects.append(Car(pos: Vector3(-Float(Level.tilesPerRow), -3.0, -Float(rowIndex) * 2), speed: row.speed));
+                gameObjects.append(Car(pos: Vector3(sectionWidth * Float(i) - Float(Level.tilesPerRow), -3.0, -Float(rowIndex) * 2), speed: row.speed));
             }
         case "water":
             // Create and append lilypad object
